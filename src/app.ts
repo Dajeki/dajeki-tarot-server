39b4dd8418185097955d72275e372ec0cb9985e9
Zachary A. K <ZKDajeki@gmail.com>
2021-04-08 22:16:01 -0400

    :goal_net: Added Error catching from a lack of set .env variables

--- src/app.ts
+++ src/app.ts
@@ -35,78 +35,93 @@ const optionsLimiter: rateLimit.Options = {
 //Subscribe the rate limiter middleware for random cards endpoint
 app.use( "/cards/:amount", rateLimit( optionsLimiter ));
 
-const PORT = 8080;
-const googleClientId = process.env.GOOGLE_CLIENT_ID;
+const PORT = parseInt( process.env.PORT || "0" );
+const { GOOGLE_CLIENT_ID, DATABASE_URL } = process.env;
 
-const client = new OAuth2Client( googleClientId );
+if( PORT && GOOGLE_CLIENT_ID && DATABASE_URL ) {
 
-const dbConnectionPool = new Pool({
-	connectionString: process.env.DATABASE_URL,
-	ssl             : {
-		rejectUnauthorized: false,
-	},
-});
+	const client = new OAuth2Client( GOOGLE_CLIENT_ID );
 
-/**
- * Cards db access.
- * :amount - should be the number of random cards you would like returned.
- * request is returned as as application/json
- */
-app.get( "/cards/:amount", ( req, res ) => {
-	( async function () {
-		const requestAmount = parseInt( req.params.amount );
-		if ( requestAmount > 78 ) {
-			res.send( "{ \"error\" : \"There are only 78 cards in a Tarot Deck.\"}" );
-			return;
-		}
-		else if ( requestAmount < 1 ) {
-			res.send( "{ \"error\" : \"Please ask for at least 1 card.\"}" );
-			return;
-		}
+	const dbConnectionPool = new Pool({
+		connectionString: DATABASE_URL,
+		ssl             : {
+			rejectUnauthorized: false,
+		},
+	});
 
-		const dbClient = await dbConnectionPool.connect();
+	/**
+	 * Cards db access.
+	 * :amount - should be the number of random cards you would like returned.
+	 * request is returned as as application/json
+	 */
+	app.get( "/cards/:amount", ( req, res ) => {
+		( async function () {
+			const requestAmount = parseInt( req.params.amount );
+			if ( requestAmount > 78 ) {
+				res.send( "{ \"error\" : \"There are only 78 cards in a Tarot Deck.\"}" );
+				return;
+			}
+			else if ( requestAmount < 1 ) {
+				res.send( "{ \"error\" : \"Please ask for at least 1 card.\"}" );
+				return;
+			}
 
-		const queryParamsForID = queryCardByID( returnRandomSelectedSet( requestAmount ));
-		const queryResults = await dbClient.query( queryParamsForID );
+			const dbClient = await dbConnectionPool.connect();
 
-		dbClient.release();
+			const queryParamsForID = queryCardByID( returnRandomSelectedSet( requestAmount ));
+			const queryResults = await dbClient.query( queryParamsForID );
 
-		console.log( `\nReturned ${ queryResults.rows.length } rows.` );
+			dbClient.release();
 
-		res.setHeader( "content-type", "application/json; charset=utf-8" );
-		res.send( JSON.stringify( queryResults.rows ));
-	})();
-});
+			console.log( `\nReturned ${ queryResults.rows.length } rows.` );
 
-/**
- * Login endpoint
- * Currently verifies only Google 0Auth2 JWT -  https://developers.google.com/identity/sign-in/web/backend-auth#verify-the-integrity-of-the-id-token
- * TODO: maintain sub and full name in the backend for tarot spread information retreival in the frontend.
- */
-app.get( "/userInfo/login", ( req, res ) => {
-	( async function () {
-		try {
-			const ticket = await client.verifyIdToken({
-				idToken : req.headers.authorization?.split( " " )[1] || "",
-				audience: googleClientId,
-			});
+			res.setHeader( "content-type", "application/json; charset=utf-8" );
+			res.send( JSON.stringify( queryResults.rows ));
+		})();
+	});
 
-			const payload: TokenPayload | undefined = ticket.getPayload();
+	/**
+	 * Login endpoint
+	 * Currently verifies only Google 0Auth2 JWT -  https://developers.google.com/identity/sign-in/web/backend-auth#verify-the-integrity-of-the-id-token
+	 * TODO: maintain sub and full name in the backend for tarot spread information retreival in the frontend.
+	 */
+	app.get( "/userInfo/login", ( req, res ) => {
+		( async function () {
+			try {
+				const ticket = await client.verifyIdToken({
+					idToken : req.headers.authorization?.split( " " )[1] || "",
+					audience: GOOGLE_CLIENT_ID,
+				});
 
-			console.log( "payload:", payload );
-			console.log( `User ${ payload?.name } verified` );
+				const payload: TokenPayload | undefined = ticket.getPayload();
 
-			const { sub, email, name, picture } = payload as TokenPayload;
-			const userId = sub;
+				console.log( "payload:", payload );
+				console.log( `User ${ payload?.name } verified` );
 
-			res.send( `${ userId }\n${ email }\n${ name }\n<img src="${ picture }"/>` );
-		}
-		catch {
-			res.send( "Login Information Malformed" );
-		}
-	})();
-});
+				const { sub, email, name, picture } = payload as TokenPayload;
+				const userId = sub;
 
-app.listen( PORT, () => {
-	console.log( `⚡️[server]: Server is running at https://localhost:${ PORT }` );
-});
+				res.send( `${ userId }\n${ email }\n${ name }\n<img src="${ picture }"/>` );
+			}
+			catch {
+				res.send( "Login Information Malformed" );
+			}
+		})();
+	});
+
+
+	app.listen( PORT, () => {
+		console.log( `⚡️[server]: Server is running at https://localhost:${ PORT }` );
+	});
+
+}
+else {
+
+	let errorString = "";
+	errorString += PORT ? "PORT\n" : "";
+	errorString += GOOGLE_CLIENT_ID ? "GOOGLE_CLIENT_ID\n" : "";
+	errorString += DATABASE_URL ? "DATABASE_URL\n" : "";
+
+	throw new Error( `${ errorString }Please set the above .env variables in the project's .env file.` );
+
+}
